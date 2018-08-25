e28307a87a6b9e8520e303182cc50e5dd07e9620
Nick <Nick@Nick-AMD-8320>
2018-08-25 13:49:45 -0500

    initial commit

---
+++ src/main/java/ModMain.java
@@ -0,0 +1,10 @@
+package net.nikolateslax.mods.discordsync;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "DiscordSync", version = "0.0.1.0", useMetadata = true, serverSideOnly = true, acceptedMinecraftVersions =  "[1.6.4,1.7.10]")
+public class ModMain {
+	
+}
